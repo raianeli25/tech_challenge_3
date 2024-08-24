aaa0c8da02739f2095a0bfa71ff1ce7b5372b268
Chrystian Remes <chrystian@windowslive.com>
2024-08-24 19:48:30 -0300

    First Attempt to Connect Database via API

--- src/main.py
+++ src/main.py
@@ -1,7 +1,21 @@
 import logging
 
+import pandas as pd
+
 from fastapi import FastAPI
-import psycopg2
+
+from interfaces.db_definitions import DatabaseDefs
+from interfaces.db_interfaces import DBInterface
+
+db_defs = DatabaseDefs()
+db_interface = DBInterface(
+    user=db_defs.POSTGRES_USER,
+    password=db_defs.POSTGRES_PASSWORD,
+    db_name=db_defs.POSTGRES_DB,
+    hostname=db_defs.POSTGRES_HOST
+)
+
+CSV_FILE = ".data/spotify_raw_dataset.csv"
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)s %(message)s',
@@ -12,27 +26,32 @@ logging.basicConfig(
     )
 
 logging.info('############## STARTING MAIN ##################')
+
 app = FastAPI()
 
 logging.info('Created instances of FatAPI')
 
 @app.get("/")
 async def home():
-
     return "Hello World!!"
 
 @app.get("/connect_db")
 async def connect_db():
-    
     try:
-
-        conn = psycopg2.connect("host=db dbname=postgres user=postgres password=postgres")
-
-        return "Conexão OK"
-    
+        conn_engine = db_interface.get_connection_engine()
+        conn_engine.connect()
+        return "Connection Passed"
     except:
-
-        return "Conexão FAIL"
+        return "Connection Failed"
+    
+@app.get("/load_data_to_db")
+async def connect_db():
+    try:
+        spotify_df = pd.read_csv(CSV_FILE)
+        conn_engine = db_interface.get_connection_engine()
+        return spotify_df.to_dict()
+    except:
+        return "Ops! Something went wrong!"
 
 
 
